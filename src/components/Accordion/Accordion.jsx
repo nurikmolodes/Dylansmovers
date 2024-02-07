bb654faa10b41a8ce89ce78079ded0c365bae760
Nurik <102224735+nurikmolodes@users.noreply.github.com>
2024-02-07 17:03:42 -0500

    see

---
+++ src/components/Accordion/Accordion.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import "./Accordion.scss"; // import your SCSS file for styling
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
+
+const Accordion = ({ items }) => {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const onItemClick = (index) => {
+    setActiveIndex(index === activeIndex ? null : index);
+  };
+
+  return (
+    <div className="accordion-container">
+      {items.map((item, index) => (
+        <div key={index} className="accordion-item">
+          <div
+            className={`accordion-title ${index === activeIndex ? "active" : ""}`}
+            onClick={() => onItemClick(index)}>
+            {item.title}
+            <FontAwesomeIcon
+              icon={index === activeIndex ? faChevronUp : faChevronDown}
+              className="accordion-arrow"
+            />
+          </div>
+          {index === activeIndex && <div className="accordion-content">{item.content}</div>}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Accordion;
